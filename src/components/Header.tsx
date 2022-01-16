3a7bbe5b9208e55a39c73109cc8c9ca3c20e73cc
kajikentaro <k.kentarou0412@gmail.com>
2022-01-16 15:22:22 +0000

    Add: ヘッダーの作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,22 @@
+import logo from "img/logo.png";
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "styles/Header.module.scss";
 
 const Header: NextPage = () => {
   return (
     <header className={styles.header}>
       <div className={styles.content}>
-        <div className={styles.icon} />
+        <Link href="/">
+          <div className={styles.icon}>
+            <Image src={logo} height={60} width={271} />
+          </div>
+        </Link>
+        <div className={styles.operation_btn}>
+          <Link href="/">サイトトップ</Link>
+          <Link href="/list">一覧</Link>
+        </div>
       </div>
     </header>
   );

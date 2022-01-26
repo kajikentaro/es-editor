dd75ad3e3af279c206bfd86db9f00c0c77423d64
kajikentaro <k.kentarou0412@gmail.com>
2022-01-26 08:44:25 +0000

    Add: データアップロードのテストコード追加

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,13 +1,43 @@
 import { NextPage } from "next";
 import Link from "next/link";
+import { backup } from "utils/verify";
 
 const Login: NextPage = () => {
-  if(!process.env.NEXT_PUBLIC_BACKEND_URL)throw new Error("環境変数が定義されていません")
+  const LOGIN_URL = process.env.NEXT_PUBLIC_LOGIN_URL;
+  const REST_URL = process.env.NEXT_PUBLIC_REST_URL;
+  if (!LOGIN_URL || !REST_URL) {
+    throw new Error("環境変数が定義されていません");
+  }
+
+  const handleDownload = async () => {
+    const param = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username: "Hoge" }),
+    };
+    const res = await fetch(REST_URL, param);
+    const status = await res.json();
+    alert(status);
+  };
+
+  const handleUpload = async () => {
+    const param = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: backup(),
+    };
+    const res = await fetch(REST_URL, param);
+    const status = await res.json();
+    alert(status);
+  };
+
   return (
     <>
-    <Link href={process.env.NEXT_PUBLIC_BACKEND_URL}>
-      <a>ログイン</a>
-    </Link>
+      <Link href={LOGIN_URL}>
+        <a>ログイン</a>
+      </Link>
+      <button onClick={handleUpload}>アップロード</button>
+      <button onClick={handleDownload}>ダウンロード</button>
     </>
   );
 };

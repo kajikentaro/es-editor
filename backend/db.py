d180f0c146c3076d2e8152d4dfcc4b5f2779dc5b
kajikentaro <k.kentarou0412@gmail.com>
2022-02-01 04:21:04 +0000

    wip

--- backend/db.py
+++ backend/db.py
@@ -1,17 +1,27 @@
 import os
 
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+load_dotenv(verbose=True)
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://{user}:{password}@{host}/{db_name}".format(**{
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{user}:{password}@{host}/{db_name}".format(**{
     'user': os.environ.get('RDS_USER', None),
     'password': os.environ.get('RDS_PASS', None),
     'host': os.environ.get('RDS_HOST', None),
     'db_name': os.environ.get('RDS_DB_NAME', None)
 })
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY") or os.urandom(24)
+
+
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
+
 
 
 class User(db.Model):

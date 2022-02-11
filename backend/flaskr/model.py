85f86a90335cd647f939496da276da76807b8835
kajikentaro <k.kentarou0412@gmail.com>
2022-02-11 07:38:33 +0000

    Update: モジュール分割
    blueprintを使ってルーティング設定を分散
    その他db, configもファイル分け

---
+++ backend/flaskr/model.py
@@ -0,0 +1,15 @@
+
+from flask_login import (LoginManager, UserMixin, current_user, login_required,
+                         login_user, logout_user)
+
+
+class User(UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    unique_id = db.Column(db.String(120), unique=True)
+    username = db.Column(db.String(120))
+    email = db.Column(db.String(120))
+
+    def __init__(self, unique_id, username, email):
+        self.unique_id = unique_id
+        self.username = username
+        self.email = email

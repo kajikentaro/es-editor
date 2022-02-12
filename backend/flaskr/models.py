9f8333147b04a09a3b921de294e9f541dbffa72a
kajikentaro <k.kentarou0412@gmail.com>
2022-02-12 03:46:25 +0000

    Add: 各種ドキュメント関連のモデルを追加
    document 文書情報
    tag 項目情報
    company 企業情報
    document_history 文書履歴

--- backend/flaskr/models.py
+++ backend/flaskr/models.py
@@ -1,16 +1,61 @@
-from flask_login import (LoginManager, UserMixin, current_user, login_required,
-                         login_user, logout_user)
+from flask_login import UserMixin
+from sqlalchemy import BigInteger, Column, Integer, String
 
 from . import db
 
+localId = String(20)
+globalId = String(30)
+contentTitle = String(150)
+
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    unique_id = db.Column(db.String(120), unique=True)
-    username = db.Column(db.String(120))
-    email = db.Column(db.String(120))
+    id = Column(Integer, primary_key=True)
+    userid = Column(globalId, unique=True)
+    username = Column(contentTitle)
+    email = Column(String(150))
 
-    def __init__(self, unique_id, username, email):
-        self.unique_id = unique_id
+    def __init__(self, userid, username, email):
+        self.userid = userid
         self.username = username
         self.email = email
+
+
+class Tag(db.Model):
+    uniqueid = Column(Integer, autoincrement=True, primary_key=True)
+    userid = Column(globalId)
+    id = Column(localId)
+    name = Column(contentTitle)
+    updateDate = Column(BigInteger)
+
+
+class Company(db.Model):
+    uniqueid = Column(Integer, autoincrement=True, primary_key=True)
+    userid = Column(globalId)
+    id = Column(localId)
+    name = Column(contentTitle)
+    updateDate = Column(BigInteger)
+
+
+class Document(db.Model):
+    uniqueid = Column(Integer, autoincrement=True, primary_key=True)
+    userid = Column(globalId)
+    id = Column(localId)
+    name = Column(String(150))
+    companyId = Column(localId)
+    tagId = Column(localId)
+    text = Column(String(10000))
+    wordCount = Column(Integer)
+    updateDate = Column(BigInteger)
+
+
+class DocumentHistory(db.Model):
+    uniqueid = Column(Integer, autoincrement=True, primary_key=True)
+    userid = Column(globalId)
+    id = Column(globalId)
+    documentId = Column(localId)
+    name = Column(String(150))
+    companyId = Column(localId)
+    tagId = Column(localId)
+    text = Column(String(10000))
+    wordCount = Column(Integer)
+    updateDate = Column(BigInteger)

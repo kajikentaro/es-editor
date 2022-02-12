7597ca3dcb3e002e544f9c682bdfb78730e3149b
kajikentaro <k.kentarou0412@gmail.com>
2022-02-12 10:05:09 +0000

    Add: document list取得処理

--- backend/flaskr/rest_api/document.py
+++ backend/flaskr/rest_api/document.py
@@ -4,11 +4,12 @@ from locale import currency
 from pydoc import doc
 from unicodedata import name
 
-from flask import Blueprint, Response, request
+from flask import Blueprint, Response, jsonify, request
 from flask_login import current_user, login_required
+from itsdangerous import json
 
 from .. import db
-from ..models import Document
+from ..models import Document, DocumentSchema
 
 bp = Blueprint('document', __name__, url_prefix='/document')
 
@@ -16,12 +17,15 @@ bp = Blueprint('document', __name__, url_prefix='/document')
 @bp.route("/", methods=['GET'])
 @login_required
 def get_list():
-    return Response(current_user.email)
+    document_schema = DocumentSchema(many=True)
+    data = document_schema.dump(Document.query.filter_by(user_id=current_user.user_id).all())
+    return jsonify(data)
 
 
+# 新規登録orアップデート
 @bp.route("/", methods=['POST'])
 @login_required
-def regest():
+def post():
     payload = request.json
     _unix_sec = (datetime.utcnow() + timedelta(hours=9)).timestamp()
 
@@ -36,11 +40,12 @@ def regest():
     document.user_id = current_user.user_id
 
     if Document.query.filter_by(user_id=document.user_id, id=document.id).one_or_none():
-        return Response(payload.get('tagId'))
+        # TODO: アップデートして履歴に保存する処理
+        return jsonify({})
 
     db.session.add(document)
     db.session.commit()
-    return Response("登録完了")
+    return jsonify({})
 
 
 @bp.route("/<int:id>", methods=['GET'])

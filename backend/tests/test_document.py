2ad1bd55701d358bd1f8d9f0db4c5c82e2aeb3e6
saityyy <sec200091@gmail.com>
2022-02-22 11:30:19 +0000

    Change: フォーマッタ適用 ファイル名変更

--- backend/tests/test_document.py
+++ backend/tests/test_document.py
@@ -16,19 +16,20 @@ def test_db(session):
     print(tag.unique_id)
     assert tag.unique_id > 0
 
+
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
 
-def test_template(client,client2):
+
+def test_template(client, client2):
     response = client.get(
         '/',
         data=json.dumps({'a': 1, 'b': 2}),
         content_type='application/json',
     )
-
-    #assert (response.get_data(as_text=True)) == None
-    #data = json.loads(response.get_data(as_text=True))
-
+    # assert (response.get_data(as_text=True)) == None
+    # data = json.loads(response.get_data(as_text=True))
     assert response.status_code == 200
-    #assert data['sum'] == 3
+
+    # assert data['sum'] == 3

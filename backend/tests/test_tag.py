c6143df7e5e2330a0d625db0d253e837ee75a888
kajikentaro <k.kentarou0412@gmail.com>
2022-02-25 07:40:39 +0000

    Fix: Tag.queryが使えない不具合を解消

--- backend/tests/test_tag.py
+++ backend/tests/test_tag.py
@@ -1,41 +1,31 @@
-import pytest
 from flask import json
-from flaskr.models import DocumentSchema, Tag
-
-# save
-# update
-# delete
-# reload
+from flaskr.models import Tag
 
 
-def test_db(session):
+def test_write_db(session):
     tag = Tag()
-    tag.user_id = 'hoge'
-    tag.id = 'hoge'
-    tag.name = 'hoge'
+    tag.user_id = "hoge"
+    tag.id = "hoge"
+    tag.name = "hoge"
     tag.update_date = 12345
 
     session.add(tag)
     session.commit()
-
-    print(tag.unique_id)
     assert tag.unique_id > 0
 
-
-def test_home(client):
-    response = client.get('/')
-    assert response.status_code == 200
+    data = Tag.query.first()
+    assert data.name == "hoge"
 
 
 def test_template(client):
     response = client.get(
-        '/',
-        data=json.dumps({'a': 1, 'b': 2}),
-        content_type='application/json',
+        "/",
+        data=json.dumps({"a": 1, "b": 2}),
+        content_type="application/json",
     )
 
-    #assert (response.get_data(as_text=True)) == None
-    #data = json.loads(response.get_data(as_text=True))
+    # assert (response.get_data(as_text=True)) == None
+    # data = json.loads(response.get_data(as_text=True))
 
     assert response.status_code == 200
-    #assert data['sum'] == 3
+    # assert data['sum'] == 3

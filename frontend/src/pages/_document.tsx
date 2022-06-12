fdd3dc3475c57a6cf8238e31bbb7f19ea3b9e32e
kajikentaro <k.kentarou0412@gmail.com>
2022-06-12 13:19:08 +0000

    Add: google tag manager

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,8 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import { GA_TRACKING_ID } from "utils/gtag";
 import Script from "next/script";
-import Header from "components/Header";
-import Footer from "components/Footer";
+import { GA_TRACKING_ID } from "utils/gtag";
 
 class MyDocument extends Document {
   render() {
@@ -40,8 +38,28 @@ class MyDocument extends Document {
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9049003923441273"
             crossOrigin="anonymous"
           ></script>
+          {/* Google Tag Manager */}
+          <Script
+            id="google-tag-manager"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-5D8PJ49');`,
+            }}
+          />
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5D8PJ49"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
           <main>
             <Main />
             <NextScript />

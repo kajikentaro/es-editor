5a13a3d075e5efd816800656887516ae774eccc6
kajikentaro <k.kentarou0412@gmail.com>
2022-07-03 05:15:06 +0000

    Add: ファビコンを追加

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -13,7 +13,29 @@ class MyDocument extends Document {
             name="description"
             content="バージョン管理、検索機能、企業・項目タグ別管理、文字数カウント、クラウド同期。豊富な機能がWebサイトで実現。就活ESエディター"
           />
-          <link rel="icon" href="/favicon.ico" />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#5bbad5" />
+          <meta name="theme-color" content="#ffffff" />
+
           {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
           {GA_TRACKING_ID && (
             <>
